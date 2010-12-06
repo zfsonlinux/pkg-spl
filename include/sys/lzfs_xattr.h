1f53a555bbccf0ef0e7e377c66621df551b11d46
Rohan Puri <rohan@kqinfotech.com>
2010-12-06 18:22:04 +0530

    lzfs_xattr.h added, also some function decl related to EA added

---
+++ include/sys/lzfs_xattr.h
@@ -0,0 +1,22 @@
+#ifndef _LZFS_XATTR_H
+#define _LZFS_XATTR_H
+
+extern struct xattr_handler *lzfs_xattr_handlers[];
+
+ssize_t
+lzfs_listxattr(struct dentry *dentry, char *buffer, size_t size);
+
+extern struct xattr_handler lzfs_xattr_user_handler;
+
+extern struct xattr_handler lzfs_xattr_security_handler;
+
+int
+lzfs_xattr_get(struct inode *inode, const char *name,
+		void *buffer, size_t size, int);
+int
+lzfs_removexattr(struct dentry *dentry, const char *name);
+
+int
+lzfs_init_security(struct inode *inode, struct inode *dir);
+#endif /* _LZFS_XATTR_H */
+
